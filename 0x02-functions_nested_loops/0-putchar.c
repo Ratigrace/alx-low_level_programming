384fe27f09f289fd11c81308f2bbbd7649f5515b
Ratigrace <chijongwegrace@gmail.com>
2023-02-21 07:48:35 -0800

    putchar main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include<stdio.h>
-#include<main.h>
-/**
- * main -A program that prints putchar.
- *
- * Return: Always 0(Success)
- */
-int main(void)
-{
-	putchar ("_putchar\n");
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * created by Grace
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+	}
+
