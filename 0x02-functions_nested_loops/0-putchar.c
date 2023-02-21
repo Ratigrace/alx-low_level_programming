bd59486dc3720245c058ed8f921a17bd7d4a7ba6
Ratigrace <chijongwegrace@gmail.com>
2023-02-21 06:31:26 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -A program that prints putchar.
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	putchar ("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning C function and nested loops
