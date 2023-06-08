274d7edc393f8e7ae2a2c1707fd36e94cdc38cbe
Ratigrace <chijongwegrace@gmail.com>
2023-06-08 13:51:04 -0700

    Power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A function that prints an exponential
+ * @x: A base number
+ * @y: The exponent
+ *
+ * Return: Number rised by its exponents
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
