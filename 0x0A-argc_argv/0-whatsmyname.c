d0c879dfeed43ce98f9ba17f562273ecf9b1de39
Ratigrace <chijongwegrace@gmail.com>
2023-06-09 12:38:39 -0700

    comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 
 /**
- * @argc: number of arguments
+ * main - printing a file name
  * @argv: array of arguments
  *
+ * @argc: number of arguments
+ *
+ * main - printing file name
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc ==1)
+	if (argc < 2)
 		printf("%s\n", argv[0]);
 
 	return (0);
