906c4f0d890eea3d13826d4fff7e8096b7ef5566
Ratigrace <chijongwegrace@gmail.com>
2023-06-29 17:01:23 -0700

    a function that prints a message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_first - A function that prints a message
+ *
+ * Return: Nothing
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
