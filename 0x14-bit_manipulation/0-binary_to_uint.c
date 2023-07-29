8581ccab82b9db81cf4364a2ab2fd433f066388d
Ratigrace <chijongwegrace@gmail.com>
2023-07-29 01:10:13 -0700

    binary unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,48 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * binary_to_uint - a function that converts a bin num to an un int
- * @b: string containing the binary number
- *
- * Return: unsigned int
- */
+* _strlen - find the length of a string
+* @s: pointer to the string to check
+* Return: void
+*/
+
+
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
+
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: binary number
+*
+* Return: 0 or converted number
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base_ten;
+	unsigned int n = 0;
+	int i, len;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b != '\0')
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
+	len = _strlen(b);
 
-		base_ten = (base_ten * 2) + (*b - '0');
-			b++;
+	for (i = 0; i != len; i++)
+	{
+		if (b[len - i - 1] == '1')
+			n += 1 << i;
+		else if (b[len - i - 1] != '0')
+			return (0);
 	}
-	return (base_ten);
+
+	return (n);
 }
