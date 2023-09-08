4cc7b241f263a886c268c158c95da37840decdbd
Ratigrace <chijongwegrace@gmail.com>
2023-09-08 15:36:28 -0700

    update hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,25 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - update  or add an element in a hash table.
+ * hash_table_set - update an elements in a hash table.
  * @ht: pointer to the hash table.
- * @key: key to add - cannot be an empty string.
- * @value: value at key.
+ * @key: key to add and cannot be an empty string.
+ * @value: The value associated with key.
  *
  * Return: Success - 1.
- *         Failure - 0.
+ *         Failure - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
+	hash_node_t *new_table;
+	char *update;
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	update = strdup(value);
+	if (update == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -28,7 +28,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = update;
 			return (1);
 		}
 	}
+
+	new_table = malloc(sizeof(hash_node_t));
+	if (new_table == NULL)
+	{
+		free(update);
+		return (0);
+	}
+	new_table->key = strdup(key);
+	if (new_table->key == NULL)
+	{
+		free(new_table);
+		return (0);
+	}
+	new_table->value = update;
+	new_table->next = ht->array[index];
+	ht->array[index] = new_table;
+
+	return (1);
+}
