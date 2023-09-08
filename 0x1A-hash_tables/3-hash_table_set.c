e20c877be7b21211674f379a5eddd1ee36a8704b
Ratigrace <chijongwegrace@gmail.com>
2023-09-08 15:12:39 -0700

    print hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - update  or add an element in a hash table.
+ * @ht: pointer to the hash table.
+ * @key: key to add - cannot be an empty string.
+ * @value: value at key.
+ *
+ * Return: Success - 1.
+ *         Failure - 0.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
